0429cd17437e40a5cda8cebcf265bf080e0d86ca
liuli <liulirun@gmail.com>
2023-10-09 17:58:56 -0400

    update

--- mysql/backup.py
+++ mysql/backup.py
@@ -4,27 +4,27 @@ import docker
 class Backup:
     def __init__(self):
         self.IF_DEBUG = False
-        self.docker_client = docker.from_env()
+        self.client = docker.from_env()
 
     def run(self):
         self.backup_mysql()
         self.push_mysql_image()
 
     def backup_mysql(self, container_name="mysql-stock2"):
-        for container in self.docker_client.containers.list():
+        for container in self.client.containers.list():
             if (container.name == container_name):
-                c = self.docker_client.containers.get(container.short_id)
+                c = self.client.containers.get(container.short_id)
                 res = c.exec_run(
                     '/bin/sh -c "rm /var/lib/mysql/bk_stock.sql && mysqldump -u root -pmysql_pass --databases stock > /var/lib/mysql/bk_stock.sql && ls /var/lib/mysql/bk_stock.sql"')
                 if self.IF_DEBUG:
                     print(res)
 
     def push_mysql_image(self, image_name="liulirun/mysql-stock2"):
-        container = self.docker_client.containers.get('mysql-stock2')
+        container = self.client.containers.get('mysql-stock2')
         container.commit('mysql-stock2', tag='latest')
-        for line in self.docker_client.images.push(image_name, tag='latest', stream=True, decode=True):
-            print(line)
-
+        # for line in self.client.images.push(image_name, tag='latest', stream=True, decode=True):
+        #     print(line)
+        print('docker updated')
 
 if __name__ == "__main__":
     b = Backup()
